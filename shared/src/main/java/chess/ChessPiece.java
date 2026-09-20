349ed9095faa24d2a212d5a520475fca6fb01004
dvd-berry <dberry1654@gmail.com>
2026-09-19 18:09:52 -0600

    Added overrides for ChessPiece

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 import chess.ChessGame.TeamColor;
 import chess.ChessPiece.PieceType;
@@ -21,6 +22,20 @@ public class ChessPiece {
         color = pieceColor;
         this.type = type;
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessPiece piece = (ChessPiece) o;
+        return color == piece.color && type == piece.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(color, type);
     }
 
     /**
